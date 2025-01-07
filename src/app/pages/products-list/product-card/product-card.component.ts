e06f65d967a1f3953e5f94b201f1e3efd6f86ac1
siddPandey <pd.siddharth@gmail.com>
2025-01-07 12:28:25 -0500

    Card populated with data

--- src/app/pages/products-list/product-card/product-card.component.ts
+++ src/app/pages/products-list/product-card/product-card.component.ts
@@ -1,13 +1,30 @@
 import { Component, input } from '@angular/core';
 import { Product } from '../../../models/products.model';
+import { PrimaryButtonComponent } from "../../../components/primary-button/primary-button.component";
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   template: `
-    <p>
-      {{product().title}} 
-    </p>
+    <div class="bg-white p-6 shadow-md rounded-lg border flex flex-col relative"> 
+      <div class="mx-auto">  
+        <img [src]="product().image" class="w-[200px] h-[100px] object-contain" />
+      </div>
+      <div class="flex flex-col mt-2">
+        <span class="text-md font-bold">{{product().title}}</span>  
+        <span class="text-sm ">{{"$"+product().price}}</span>  
+        <app-primary-button label="Add to cart" class="mt-3"/>
+      </div>
+      <span class="absolute top-2 right-3 text-sm font-bold"
+      [class] = "product().stock ? 'text-green-500' : 'text-red-500'">  
+         @if (product().stock) {
+          {{product().stock}} left  
+         }@else {
+          Out of stock
+         }
+      </span>
+
+  </div>
   `,
   styles: ``
 })
--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -14,8 +14,7 @@ export type Product = {
   imports: [ProductCardComponent],
   template: `
   <div>
-    <h1>Products</h1>
- 
+
       <div class="p-8 grid grid-cols-2 gap-4">
         @for (product of products(); track product.id) {
           <app-product-card [product]="product" /> <!--Right side "product" is coming from for loop item -product --> 
