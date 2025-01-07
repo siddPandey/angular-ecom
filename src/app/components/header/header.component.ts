58389a254d149b5886274e9fed5f222ee2e15575
siddPandey <pd.siddharth@gmail.com>
2025-01-07 10:16:05 -0500

    Button label made Dynamic

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,11 +7,14 @@ import { PrimaryButtonComponent } from "../primary-button/primary-button.compone
   template: `
     <div class="bg-slate-500 px-4 py-3 shadow-md flex justify-between">    
       <span>My Store</span> 
-      <app-primary-button label="Cart" (btnClicked)="showButtonClicked()"/>
+      <app-primary-button [label]="cart()" (btnClicked)="showButtonClicked()"></app-primary-button>
+    </div>
   `,
   styles: ``
 })
 export class HeaderComponent {
+
+  cart = signal('Cart');
   showButtonClicked() {
     console.log('clicked');
   } 
