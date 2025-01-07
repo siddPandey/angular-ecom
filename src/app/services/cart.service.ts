58a4e7d52a9bd4fdbe964410aba05596973b0810
siddPandey <pd.siddharth@gmail.com>
2025-01-07 16:10:36 -0500

    remove test data for cart

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -7,20 +7,20 @@ import { Product } from '../models/products.model';
 export class CartService {
 
   cart = signal<Product[]>([
-    {
-      id: 1,
-      title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
-      price: 109.95,
-      image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-      stock: 10,
-    },
-    {
-      id: 2,
-      title: 'Mens Casual Premium Slim Fit T-Shirts ',
-      price: 22.3,
-      image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
-      stock: 0,
-    }
+    // {
+    //   id: 1,
+    //   title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+    //   price: 109.95,
+    //   image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+    //   stock: 10,
+    // },
+    // {
+    //   id: 2,
+    //   title: 'Mens Casual Premium Slim Fit T-Shirts ',
+    //   price: 22.3,
+    //   image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+    //   stock: 0,
+    // }
   ]); 
 
   addtoCart(product: Product) {
